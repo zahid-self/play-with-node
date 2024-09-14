e47d2764524e8a7e96b55f11f08e99e350f57f4f
zahidself <mdjahidhasan920@gmail.com>
2024-09-14 09:35:18 +0600

    save scraped file to the local machine

--- node-cli/src/commands.js
+++ node-cli/src/commands.js
@@ -1,9 +1,32 @@
 import puppeteer from 'puppeteer';
+import { writeFile } from "node:fs/promises";
+
+function getFileName(url) {
+  const fileNameArr = url.split("//");
+  const fileName = fileNameArr[1].replace(/\//g, "");
+  return fileName;
+}
 
 export const getTitle = async (url) => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto(url);
-  const title = await page.title();
-  return title
+  try {
+    await page.goto(url);
+    const title = await page.title();
+    const fileName = getFileName(url);
+    const filePath = new URL(`../public/${fileName}.json`, import.meta.url);
+    try {
+      await writeFile(filePath, JSON.stringify(await page.content(), null, 2), { flag: 'wx' });
+    } catch (error) {
+      if (error.code === "EEXIST") {
+        await writeFile(filePath, JSON.stringify(await page.content(), null, 2));
+      }
+    }
+    return title
+  } catch (error) {
+    console.log(error)
+  } finally {
+    await page.close();
+    await browser.close();
+  }
 }
